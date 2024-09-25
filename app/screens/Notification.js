4cc2603fe3fb1b96fa3c83a16c720c1b2bd6c59f
Jeongwha Kang <jhk5242@duksung.ac.kr>
2024-09-25 21:19:36 +0900

    공지사항 api 연동

--- app/screens/Notification.js
+++ app/screens/Notification.js
@@ -1,26 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { theme } from "@assets/Theme";
+import * as Keychain from 'react-native-keychain';
 
-const notices = [
-    { id: 1, title: "캣 페스타 일정 변경 관련 공지", author: "관리 회장", date: "2024.07.06" },
-    { id: 2, title: "캣 페스타 시간 변경", author: "관리 회장", date: "2024.07.05" },
-    { id: 3, title: "캣 페스타 장소 안내", author: "관리 회장", date: "2024.07.04" },
-    { id: 4, title: "새로운 공지사항입니다.", author: "관리 회장", date: "2024.07.03" },
-    { id: 5, title: "이전 공지사항 다시 확인해주세요", author: "관리 회장", date: "2024.07.02" },
-    { id: 6, title: "캣 페스타 관련 추가 안내", author: "관리 회장", date: "2024.07.01" },
-    { id: 7, title: "공지사항 테스트", author: "관리 회장", date: "2024.06.30" },
-    { id: 8, title: "공지사항 테스트2", author: "관리 회장", date: "2024.06.29" },
-];
+// const notices = [
+//     { id: 1, title: "캣 페스타 일정 변경 관련 공지", author: "관리 회장", date: "2024.07.06" },
+//     { id: 2, title: "캣 페스타 시간 변경", author: "관리 회장", date: "2024.07.05" },
+//     { id: 3, title: "캣 페스타 장소 안내", author: "관리 회장", date: "2024.07.04" },
+//     { id: 4, title: "새로운 공지사항입니다.", author: "관리 회장", date: "2024.07.03" },
+//     { id: 5, title: "이전 공지사항 다시 확인해주세요", author: "관리 회장", date: "2024.07.02" },
+//     { id: 6, title: "캣 페스타 관련 추가 안내", author: "관리 회장", date: "2024.07.01" },
+//     { id: 7, title: "공지사항 테스트", author: "관리 회장", date: "2024.06.30" },
+//     { id: 8, title: "공지사항 테스트2", author: "관리 회장", date: "2024.06.29" },
+// ];
+
+const URL="https://www.pumble.site/api/community/1/notice/list?page=1"
+
+const getAccessToken = async () => {
+    try {
+        const credentials = await Keychain.getInternetCredentials("AccessToken");
+        if (credentials) {
+            //console.log("AccessToken:", credentials.password);
+            return credentials.password; // AccessToken 반환
+        } else {
+            console.log('No access token found');
+        }
+    } catch (error) {
+        console.error('Error retrieving access token:', error);
+    }
+};
+
+const getRefreshToken = async () => {
+    try {
+        const credentials = await Keychain.getInternetCredentials("RefreshToken");
+        if (credentials) {
+            //console.log("RefreshToken:", credentials.password);
+            return credentials.password; // RefreshToken 반환
+        } else {
+            console.log('No refresh token found');
+        }
+    } catch (error) {
+        console.error('Error retrieving refresh token:', error);
+    }
+};
+
 
 const isAdmin = false; // 관리자 모드를 제어하는 플래그
 
+
+const fetchData = async () => {
+    const token= await getAccessToken();
+    try {
+        const response = await fetch(URL, {
+            method: 'GET', // 생략 가능
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}` // 필요하다면 토큰 추가
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json(); // 응답을 JSON으로 변환
+        //console.log('Response Data:', data.result.noticeList.noticeList);
+        return(data);
+
+    } catch (error) {
+        console.error('Fetch Error:', error);
+    }
+};
+
 const Noticefication = () => {
     const navigation = useNavigation();
+    const [notiList, setNotiList]=useState([])
 
-    const handleNoticePress = (notice) => {
-        navigation.navigate('NoticeDetail', { notice });
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            const data = await fetchData(); 
+
+            setNotiList(data.result.noticeList); 
+            
+        };
+        
+        fetchNotifications(); // 비동기 함수 호출
+        console.log(notiList)
+    }, []);
+    
+
+    const handleNoticePress = (noticeId) => {
+        console.log(noticeId);
+        navigation.navigate('NoticeDetail', { noticeId });
     };
 
     const handleWritePress = () => {
@@ -33,24 +105,30 @@ const Noticefication = () => {
                 <TouchableOpacity style={styles.mainNotice} onPress={() => handleNoticePress(notices[0])}>
                     <View style={styles.mainNoticeContent}>
                         <Image source={require('../assets/Icons/megaphone.png')} style={styles.megaphoneIcon} />
-                        <Text style={styles.mainNoticeText}>{notices[0].title}</Text>
+                        {/*<Text style={styles.mainNoticeText}>{notiList[0].notice.title}</Text>*/}
+                        <Text style={styles.mainNoticeText}>공지사항 입니다.</Text>
                         <Image source={require('../assets/Icons/arrow-Down.png')} style={styles.arrowDownIcon} />
                     </View>
                 </TouchableOpacity>
 
                 {/* 나머지 공지사항들을 표시 */}
-                {notices.slice(1).map((notice) => (
-                    <TouchableOpacity key={notice.id} style={styles.noticeItem} onPress={() => handleNoticePress(notice)}>
-                        <View style={styles.noticeContent}>
-                            <View style={styles.noticeTextContainer}>
-                                <Text style={styles.noticeTitle}>{notice.title}</Text>
-                                <Text style={styles.noticeAuthor}>{notice.author}</Text>
-                                <Text style={styles.noticeDate}>{notice.date}</Text>
+                {notiList.length===0?(
+                    <Text>공지가 없어요..ㅜㅜ</Text>
+                ):(
+                    notiList.map((notice,index) => (
+                        <TouchableOpacity key={notice.id} style={styles.noticeItem} onPress={() => handleNoticePress(notice.notice.noticeId)}>
+                            <View style={styles.noticeContent}>
+                                <View style={styles.noticeTextContainer}>
+                                    <Text style={styles.noticeTitle}>{notice.notice.title}</Text>
+                                    <Text style={styles.noticeAuthor}>{notice.writer.nickname}</Text>
+                                    <Text style={styles.noticeDate}>{notice.notice.createdAt.split('T')[0]}</Text>
+                                </View>
+                                <Image source={require('../assets/Icons/arrow-Right.png')} style={styles.arrowIcon} />
                             </View>
-                            <Image source={require('../assets/Icons/arrow-Right.png')} style={styles.arrowIcon} />
-                        </View>
-                    </TouchableOpacity>
-                ))}
+                        </TouchableOpacity>
+                    ))
+                )}
+                
             </ScrollView>
 
             {/* 관리자 모드일 때 글쓰기 버튼 표시 */}
